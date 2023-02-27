22e9bfc9a392cbb8f4c451dbbfd93009132fa85c
jagottsicher <jagottsicher@gmx.net>
2023-02-27 12:26:43 +0800

    cleaned up and added an example how to use HTML templates

--- main.go
+++ main.go
@@ -1,57 +1,38 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"net/http"
+	"text/template"
 )
 
 const portNumber = ":8080"
 
 // Home is the handler for the home page
 func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "This is the home page")
+	rendernTemplate(w, "home-page.tpml")
 }
 
 // About is the handler for the about page
 func About(w http.ResponseWriter, r *http.Request) {
-	sum := addValues(2, 2)
-	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is the about page and 2 + 2 is %d", sum))
+	rendernTemplate(w, "about-page.tpml")
+
 }
 
-// Divide divides one value into another and returns message with result
-func Divide(w http.ResponseWriter, r *http.Request) {
-	x := 100.0
-	y := 0.0
-	f, err := divideValues(x, y)
+// rendernTemplate serves as a wrapper and renders
+// a template from folder /templates to a desired writer
+func rendernTemplate(w http.ResponseWriter, tpml string) {
+	parsedTemplate, _ := template.ParseFiles("./templates/" + tpml)
+	err := parsedTemplate.Execute(w, nil)
 	if err != nil {
-		_, _ = fmt.Fprintf(w, "Error: Division by zero is not a valid operation. Error returned: %s", err)
-		return
+		fmt.Println("error parsing template:", err)
 	}
-
-	_, _ = fmt.Fprintf(w, fmt.Sprintf("%f divided by %f is %f", x, y, f))
-}
-
-// divideValues divides two floats x and y, and returns the quotient and a value of type error
-func divideValues(x, y float64) (float64, error) {
-	if y == 0 {
-		err := errors.New("Divisor is zero!")
-		return 0, err
-	}
-	result := x / y
-	return result, nil
-}
-
-// addValues adds two ints x and y, and returns the sum
-func addValues(x, y int) int {
-	return x + y
 }
 
 // main is the main function
 func main() {
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
-	http.HandleFunc("/divide", Divide)
 
 	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
 	_ = http.ListenAndServe(portNumber, nil)
