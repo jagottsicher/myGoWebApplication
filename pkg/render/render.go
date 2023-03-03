7d6fe8699249c1950b52d8d206d766488703029e
jagottsicher <jagottsicher@gmx.net>
2023-03-03 12:25:53 +0800

    added 30 using layouts now

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -7,9 +7,9 @@ import (
 )
 
 // rendernTemplate serves as a wrapper and renders
-// a template from folder /templates to a desired writer
+// a layout and a template from folder /templates to a desired writer
 func RenderTemplate(w http.ResponseWriter, tpml string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tpml)
+	parsedTemplate, _ := template.ParseFiles("./templates/"+tpml, "./templates/base-layout.tpml")
 	err := parsedTemplate.Execute(w, nil)
 	if err != nil {
 		fmt.Println("error parsing template:", err)
