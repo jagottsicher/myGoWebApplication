ef28e63ca799f6e8bba69afb961afc293ab5daf5
jagottsicher <jagottsicher@gmx.net>
2023-03-10 12:26:34 +0800

    removed pat, using now chi as router and middleware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,16 +3,19 @@ package main
 import (
 	"net/http"
 
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/jagottsicher/myGoWebApplication/pkg/config"
 	"github.com/jagottsicher/myGoWebApplication/pkg/handlers"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
+	mux := chi.NewRouter()
 
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	mux.Use(middleware.Recoverer)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 
 	return mux
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/jagottsicher/myGoWebApplication
 
 go 1.20
 
-require github.com/bmizerany/pat v0.0.0-20210406213842-e4b6760bdd6f // indirect
+require github.com/go-chi/chi/v5 v5.0.8 // indirect
