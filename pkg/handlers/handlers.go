c40aa151ae180508950404c9703630b6719385e7
jagottsicher <jagottsicher@gmx.net>
2023-03-13 13:19:00 +0800

    tested writing and reading session data

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -30,6 +30,10 @@ func NewHandlers(r *Repository) {
 
 // Home is the handler for the home page
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+
+	remoteIP := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
+
 	render.RenderTemplate(w, "home-page.tpml", &models.TemplateData{})
 }
 
@@ -39,6 +43,9 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 
 	sidekickMap := make(map[string]string)
 	sidekickMap["morty"] = "Ooh, wee!"
+
+	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
+	sidekickMap["remote_ip"] = remoteIP
 
 	// send the result or any prepared data to the template
 	render.RenderTemplate(w, "about-page.tpml", &models.TemplateData{
