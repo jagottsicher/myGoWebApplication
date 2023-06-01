f836d046d24e6bc68ce9bb09e46951ae0c2e2b8c
jagottsicher <jagottsicher@gmx.net>
2023-06-01 12:27:53 +0800

    added lec 76

--- internal/handlers/handlers_test.go
+++ internal/handlers/handlers_test.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"testing"
 )
 
@@ -28,6 +29,19 @@ var allTheHandlerTests = []struct {
 	{"make-reservation", "/make-reservation", "GET", []postData{}, http.StatusOK},
 	// {"reservation-overview", "/reservation-overview", "GET", []postData{}, http.StatusOK},
 	{"not-existing-route", "/not-existing-dummy", "GET", []postData{}, http.StatusNotFound},
+	{"post-reservation", "/reservation", "POST", []postData{
+		{key: "startingEnd", value: "2023-02-23"},
+		{key: "endingEnd", value: "2023-02-25"},
+	}, http.StatusOK},
+	{"post-reservation-json", "/reservation-json", "POST", []postData{
+		{key: "startingEnd", value: "2023-02-23"},
+		{key: "endingEnd", value: "2023-02-25"},
+	}, http.StatusOK},
+	{"post-make-reservation", "/make-reservation", "POST", []postData{
+		{key: "full_name", value: "Ricky Spanish"},
+		{key: "email", value: "me@writing-an-email.to"},
+		{key: "phone", value: "555-123-4567"},
+	}, http.StatusOK},
 }
 
 func TestAllTheHandlers(t *testing.T) {
@@ -49,7 +63,21 @@ func TestAllTheHandlers(t *testing.T) {
 				t.Errorf("%s: expected %d, got %d", test.name, test.expectedStatusCode, response.StatusCode)
 			}
 		} else {
-			// here POST request handlers tests laster
+			values := url.Values{}
+
+			for _, param := range test.params {
+				values.Add(param.key, param.value)
+			}
+
+			response, err := testServer.Client().PostForm(testServer.URL+test.url, values)
+			if err != nil {
+				t.Log(err)
+				t.Fatal(err)
+			}
+
+			if response.StatusCode != test.expectedStatusCode {
+				t.Errorf("%s: expected %d,  got %d", test.name, test.expectedStatusCode, response.StatusCode)
+			}
 		}
 	}
 }
