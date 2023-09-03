4bce9cbf57d4794b9b390677489da722af490ff0
jagottsicher <jagottsicher@gmx.net>
2023-09-03 14:11:38 +0800

    added a helper function to determinate iser_id in session and a middleware to differ between authenticated and non-authenticated user redirection

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 
+	"github.com/jagottsicher/myGoWebApplication/internal/helpers"
 	"github.com/justinas/nosurf"
 )
 
@@ -23,4 +24,16 @@ func NoSurf(next http.Handler) http.Handler {
 // SessionLoad loads saves session data for each request
 func SessionLoad(next http.Handler) http.Handler {
 	return session.LoadAndSave(next)
+}
+
+// Auth redirects non-authenticated requests
+func Auth(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !helpers.IsAuthenticated(r) {
+			session.Put(r.Context(), "error", "Log in first!")
+			http.Redirect(w, r, "/user/login", http.StatusSeeOther)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
 }
--- internal/helpers/helpers.go
+++ internal/helpers/helpers.go
@@ -25,3 +25,9 @@ func ServerError(w http.ResponseWriter, err error) {
 	app.ErrorLog.Println(trace)
 	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 }
+
+// IsAuthenticated figures determinates if an authenticated user exists in the session data
+func IsAuthenticated(r *http.Request) bool {
+	exists := app.Session.Exists(r.Context(), "user_id")
+	return exists
+}
